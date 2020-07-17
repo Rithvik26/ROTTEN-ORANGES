37fb4f1edbb2b77bd481f254fd919eb9e5dc29d3
Rithvik26 <37191570+Rithvik26@users.noreply.github.com>
2020-07-17 22:06:38 +0530

    Mean project

---
+++ app_api/models/users.js
@@ -0,0 +1,25 @@
+var mongoose = require( 'mongoose' )
+var crypto = require('crypto');
+var jwt = require('jsonwebtoken');
+
+var userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    bio: String,
+    hash: String,
+    salt: String
+});
+
+userSchema.methods.setPassword = function(password){
+    this.salt = crypto.randomBytes(16).toString('hex');
+    this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+    };
+
+    
